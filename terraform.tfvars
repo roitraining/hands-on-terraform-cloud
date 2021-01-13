80581dcc8f89baaa5b0a50360ea2627ae659c467
doug <drehnstrom@yahoo.com>
2021-01-12 21:18:12 -0500

    Changes

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+project_id = "terraform-123020"
+region = "us-central1"
+zone = "us-central1-a"
+
+project = "terraform-cloud
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "The GCP Project ID."
+  type        = string
+}
+
+variable "project" {
+  description = "Name you project."
+  type        = string
+}
+
+variable "region" {
+  type    = string
+}
+
+variable "zone" {
+  type    = string
+}
