4ad8b2f57161c7966dfd19fc25f2d12f73d09751
doug <drehnstrom@yahoo.com>
2021-01-16 08:13:50 -0500

    Changes

--- vm.tf
+++ vm.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "vm" {
-  count                  = 3
   ami                    = "ami-0be2609ba883822ec"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow-http.id, aws_security_group.allow-ssh.id]
